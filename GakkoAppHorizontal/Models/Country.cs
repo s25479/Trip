e4a2561d3c81a60d4f9fa8192347f8fc04836bde
s25479 <s25479@pjwstk.edu.pl>
2024-05-24 10:56:22 +0200

    Add models

---
+++ GakkoAppHorizontal/Models/Country.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace GakkoHorizontalSlice.Models;
+
+public partial class Country
+{
+    public int IdCountry { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Trip> IdTrips { get; set; } = new List<Trip>();
+}
